0d453c8e0c89229a57a9beac092bf904d1a2eeae
manxiaoliang <xiaoliangx.man@intel.com>
2023-02-21 05:40:00 +0000

    Add DP training failure handling Handle DP training failure at userland. Check for link status when recv KMS uevent and current and prev status is connected. If link is bad, retrigger modeset to trigger DP retraining.
    Tracked-On: OAM-105867
    Signed-off-by: manxiaoliang <xiaoliangx.man@intel.com>

--- drm/DrmConnector.cpp
+++ drm/DrmConnector.cpp
@@ -92,6 +92,15 @@ auto DrmConnector::CreateInstance(DrmDevice &dev, uint32_t connector_id,
   return c;
 }
 
+int DrmConnector::UpdateLinkStatusProperty() {
+  int ret = GetConnectorProperty(*drm_, *this, "link-status",
+                                 &link_status_property_);
+  if (!ret) {
+    ALOGW("Conn %u Could not get link-status property\n", GetId());
+  }
+  return ret;
+}
+
 int DrmConnector::UpdateEdidProperty() {
   return GetOptionalConnectorProperty(*drm_, *this, "EDID", &edid_property_)
              ? 0
@@ -188,4 +197,7 @@ void DrmConnector::SetActiveMode(DrmMode &mode) {
   active_mode_ = mode;
 }
 
+const DrmProperty &DrmConnector::link_status_property() const {
+  return link_status_property_;
+}
 }  // namespace android
