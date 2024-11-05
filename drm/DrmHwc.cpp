projectceladon/drm-hwcomposer

commit e11f723f5671dea75caa291f319925581ddfdf5d

drm_hwcomposer: Query property when checking virtual displays availability
The current implementation of "getMaxVirtualDisplayCount" function
relies on the number of writeback connectors, but doesn't consider
the availability of CRTCs. Since CRTCs can only be bound to a single
pipeline, even though there are available writeback connectors, we
might fail to create a virtual pipeline if the CRTCs are being used.

This leads to failures in the "CreateVirtualDisplay" and
"SetOutputBuffer" VTS tests because the tests expect the creation of
a virtual pipeline to work based on the positive value returned by
"getMaxVirtualDisplayCount".

The virtual display feature in drm-hwcomposer is experimental and
currently unused. To address test failures, modify the function
"getMaxVirtualDisplayCount" to return 0 unless the property
"vendor.vendor.hwc.drm.enable_virtual_display" is explicitly set to
true.

Signed-off-by: Paz Zcharya <pazz@google.com>

drm/DrmHwc.cpp
@@ -19,6 +19,9 @@
 #include "DrmHwc.h"
 
 #include <cinttypes>
+#include <string>
+
+#include <android-base/properties.h>
 
 #include "backend/Backend.h"
 #include "utils/log.h"
@@ -196,6 +199,15 @@ void DrmHwc::Dump(uint32_t *out_size, char *out_buffer) {
 }
 
 uint32_t DrmHwc::GetMaxVirtualDisplayCount() {
+  /* Virtual display is an experimental feature.
+   * Unless explicitly set to true, return 0 for no support.
+   */
+  if (!base::GetBoolProperty(std::string(
+                                 "vendor.hwc.drm.enable_virtual_display"),
+                             false)) {
+    return 0;
+  }
+
   auto writeback_count = resource_manager_.GetWritebackConnectorsCount();
   writeback_count = std::min(writeback_count, 1U);
   /* Currently, only 1 virtual display is supported. Other cases need testing */

commit 9c41b68d080445f80fa144ce4e69620da270877e

drm_hwcomposer: Fix build failure on Linux-native targets
Commit e11f723f ("drm_hwcomposer: Query property when checking virtual
displays availability") introduced a call to
android::base::GetBoolProperty, which is only available in Android.
This caused the test "hwcomposer.filegroups_build_test" to fail because
the code was no longer compiling for Linux-native targets.

Fix that by replacing "android::base::GetBoolProperty" with a new
function "property_get_bool," and define the function for both Android
and non-Android targets:
For Android, utilize the existing function from <cutils/properties.h>
For other targets, a new implementation is provided in
utils/properties.h.

Fixes: e11f723f ("drm_hwcomposer: Query property when checking virtual
displays availability")

Signed-off-by: Paz Zcharya <pazz@google.com>

drm/DrmHwc.cpp
@@ -19,12 +19,10 @@
 #include "DrmHwc.h"
 
 #include <cinttypes>
-#include <string>
-
-#include <android-base/properties.h>
 
 #include "backend/Backend.h"
 #include "utils/log.h"
+#include "utils/properties.h"
 
 namespace android {
 
@@ -202,9 +200,7 @@ uint32_t DrmHwc::GetMaxVirtualDisplayCount() {
   /* Virtual display is an experimental feature.
    * Unless explicitly set to true, return 0 for no support.
    */
-  if (!base::GetBoolProperty(std::string(
-                                 "vendor.hwc.drm.enable_virtual_display"),
-                             false)) {
+  if (0 == property_get_bool("vendor.hwc.drm.enable_virtual_display", 0)) {
     return 0;
   }
 

utils/properties.h
@@ -39,4 +39,37 @@ auto inline property_get(const char *name, char *value,
   return static_cast<int>(strlen(value));
 }
 
+/**
+ * Bluntly copied from system/core/libcutils/properties.cpp,
+ * which is part of the Android Project and licensed under Apache 2.
+ * Source:
+ * https://cs.android.com/android/platform/superproject/main/+/main:system/core/libcutils/properties.cpp;l=27
+ */
+auto inline property_get_bool(const char *key, int8_t default_value) -> int8_t {
+  if (!key)
+    return default_value;
+
+  int8_t result = default_value;
+  char buf[PROPERTY_VALUE_MAX] = {};
+
+  int len = property_get(key, buf, "");
+  if (len == 1) {
+    char ch = buf[0];
+    if (ch == '0' || ch == 'n') {
+      result = false;
+    } else if (ch == '1' || ch == 'y') {
+      result = true;
+    }
+  } else if (len > 1) {
+    if (!strcmp(buf, "no") || !strcmp(buf, "false") || !strcmp(buf, "off")) {
+      result = false;
+    } else if (!strcmp(buf, "yes") || !strcmp(buf, "true") ||
+               !strcmp(buf, "on")) {
+      result = true;
+    }
+  }
+
+  return result;
+}
+
 #endif