92afffdebb929a08d4f6907f8afb461e8776e66f
jialin <lin.a.jia@intel.com>
2025-02-05 09:39:10 +0000

    Add hdcp hwc support
    1. Add hdcp drm property
    2. Add hdcp2.2 support in hwc/libhwcservice
    3. Delete libhwcservice hardcode for servie connect
    4. Fix type0/type1 switch issue
    5. Add libhwcservice for media hdcp api
    
    Test-done:
        Android hdcp test
    
    Tracked-On: OAM-130842
    Signed-off-by: Jialin <lin.a.jia@intel.com>
    Signed-off-by: Li, HaihongX <haihongx.li@intel.com>

--- drm/ResourceManager.h
+++ drm/ResourceManager.h
@@ -23,6 +23,7 @@
 #include "DrmDisplayPipeline.h"
 #include "DrmFbImporter.h"
 #include "UEventListener.h"
+#include "hwc2_device/hwcservice.h"
 
 namespace android {
 class HwcDisplay;
@@ -64,6 +65,7 @@ class ResourceManager {
   void UpdateFrontendDisplays();
   void DetachAllFrontendDisplays();
   void ReloadNode();
+  void HwcServiceThread();
 
   std::vector<std::unique_ptr<DrmDevice>> drms_;
 
@@ -81,6 +83,8 @@ class ResourceManager {
   bool initialized_{};
   int card_num_ = 0;
   bool reloaded_{};
+  android::HwcService hwcService_;
+  std::thread pt_;
 };
 }  // namespace android
 
